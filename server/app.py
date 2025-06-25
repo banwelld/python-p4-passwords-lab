92406445d43f9a5669e1614bd081b5fb6d5d6425
Dave Banwell <dave@davebanwell.com>
2025-06-25 19:11:28 -0400

    tests passing

--- server/app.py
+++ server/app.py
@@ -28,16 +28,41 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session['user_id']
+        user = User.query.filter(User.id == user_id).first()
+        
+        if not user:
+            return {}, 204
+        
+        return user.to_dict(), 200
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        username = request.json['username']
+        user = User.query.filter(User.username == username).first()
+        
+        password = request.json['password']
+        
+        if not user.authenticate(password):
+            return user.to_dict(), 401
+        
+        session['user_id'] = user.id
+        return user.to_dict(), 200
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return [], 201
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
